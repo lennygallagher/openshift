0cc0c625b5173ca81cb3544cd2754892b6eaa99e
ADESSO\gallagher <leonhard.gallagher@adesso.ch>
2018-06-24 18:45:09 +0300

    logback and owner of docker file

--- openshift/src/main/java/ch/adesso/boundary/RestEndpoint.java
+++ openshift/src/main/java/ch/adesso/boundary/RestEndpoint.java
@@ -1,13 +1,18 @@
 package ch.adesso.boundary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("helloworld")
 public class RestEndpoint {
+    static final Logger LOG = LoggerFactory.getLogger(RestEndpoint.class);
 
     @GET
     public String helloWorld(){
+        LOG.debug("Calling helloWorld endpoint");
         return "Hello World";
     }
 }
