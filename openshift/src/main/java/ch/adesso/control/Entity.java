172adac56adbc1fa540505670230c4142256edf8
ADESSO\gallagher <leonhard.gallagher@adesso.ch>
2018-06-25 12:04:46 +0300

    persistence for application

--- openshift/src/main/java/ch/adesso/control/Entity.java
+++ openshift/src/main/java/ch/adesso/control/Entity.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
+/**
+ * Entity
+ */
 @Data
 @ToString
 @javax.persistence.Entity
